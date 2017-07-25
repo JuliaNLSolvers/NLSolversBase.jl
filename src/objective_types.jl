f5e8e7af8033ead6915d9d4b79b46aacacff8e29
Antoine Levitt <antoine.levitt@gmail.com>
2017-07-25 17:03:51 +0200

    parametrize objective types on complex or real

--- src/objective_types.jl
+++ src/objective_types.jl
@@ -3,26 +3,28 @@ real_to_complex(d::AbstractObjective, x) = iscomplex(d) ? real_to_complex(x) : x
 complex_to_real(d::AbstractObjective, x) = iscomplex(d) ? complex_to_real(x) : x
 
 # Used for objectives and solvers where no gradient is available/exists
-type NonDifferentiable{T<:Real} <: AbstractObjective
+type NonDifferentiable{T,Tcplx} <: AbstractObjective where {T<:Real,
+                                                            Tcplx<:Union{Val{true},Val{false}}  #if true, must convert back on every f call
+                                                            }
     f
     f_x::T
     last_x_f::Array{T}
     f_calls::Vector{Int}
-    iscomplex::Bool #if true, must convert back on every f call
 end
-iscomplex(obj::NonDifferentiable) = obj.iscomplex
-NonDifferentiable(f,f_x, last_x_f::Array, f_calls::Vector{Int}) = NonDifferentiable(f,f_x,last_x_f,f_calls,false) #compatibility with old constructor
+iscomplex(obj::NonDifferentiable{T,Val{true}}) where {T} = true
+iscomplex(obj::NonDifferentiable{T,Val{false}}) where {T} = false
+NonDifferentiable(f,f_x::T, last_x_f::Array, f_calls::Vector{Int}) where {T} = NonDifferentiable{T,Val{false}}(f,f_x,last_x_f,f_calls) #compatibility with old constructor
 
 function NonDifferentiable(f, x_seed::AbstractArray)
     iscomplex = eltype(x_seed) <: Complex
     if iscomplex
         x_seed = complex_to_real(x_seed)
     end
-    NonDifferentiable(f, f(x_seed), copy(x_seed), [1], iscomplex)
+    NonDifferentiable(f, f(x_seed), copy(x_seed), [1], Val{iscomplex})
 end
 
 # Used for objectives and solvers where the gradient is available/exists
-type OnceDifferentiable{T<:Real, Tgrad} <: AbstractObjective
+type OnceDifferentiable{T, Tgrad, Tcplx} <: AbstractObjective where {T<:Real, Tgrad, Tcplx<:Union{Val{true},Val{false}}}
     f
     g!
     fg!
@@ -32,10 +34,10 @@ type OnceDifferentiable{T<:Real, Tgrad} <: AbstractObjective
     last_x_g::Array{T}
     f_calls::Vector{Int}
     g_calls::Vector{Int}
-    iscomplex::Bool #if true, must convert back on every f and g call
 end
-iscomplex(obj::OnceDifferentiable) = obj.iscomplex
-OnceDifferentiable(f,g!,fg!,f_x::T, g::Tgrad, last_x_f::Array, last_x_g::Array, f_calls::Vector{Int}, g_calls::Vector{Int}) where {T, Tgrad} = OnceDifferentiable(f,g!,fg!,f_x, g, last_x_f, last_x_g, f_calls, g_calls, false) #compatibility with old constructor
+iscomplex(obj::OnceDifferentiable{T,Tgrad,Val{true}}) where {T,Tgrad} = true
+iscomplex(obj::OnceDifferentiable{T,Tgrad,Val{false}}) where {T,Tgrad} = false
+OnceDifferentiable(f,g!,fg!,f_x::T, g::Tgrad, last_x_f::Array, last_x_g::Array, f_calls::Vector{Int}, g_calls::Vector{Int}) where {T, Tgrad} = OnceDifferentiable{T,Tgrad,Val{false}}(f,g!,fg!,f_x, g, last_x_f, last_x_g, f_calls, g_calls) #compatibility with old constructor
 
 # The user friendly/short form OnceDifferentiable constructor
 function OnceDifferentiable(f, g!, fg!, x_seed::AbstractArray)
@@ -47,7 +49,7 @@ function OnceDifferentiable(f, g!, fg!, x_seed::AbstractArray)
         x_seed = complex_to_real(x_seed)
         g = complex_to_real(g)
     end
-    OnceDifferentiable(f, g!, fg!, f_val, g, copy(x_seed), copy(x_seed), [1], [1], iscomplex)
+    OnceDifferentiable{eltype(x_seed),typeof(x_seed),Val{iscomplex}}(f, g!, fg!, f_val, g, copy(x_seed), copy(x_seed), [1], [1])
 end
 # Automatically create the fg! helper function if only f and g! is provided
 function OnceDifferentiable(f, g!, x_seed::AbstractArray)
