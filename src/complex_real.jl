ecc6e2d2ffa0471293a96c04db37dd4c00b0f00d
Antoine Levitt <antoine.levitt@gmail.com>
2017-07-26 11:07:54 +0200

    document complex_real

--- src/complex_real.jl
+++ src/complex_real.jl
@@ -1,8 +1,10 @@
+"Convert a real array of size 2 x dims to a complex array of size dims"
 function real_to_complex(A::Array{T}) where T<:Real
     @assert size(A)[1] == 2
     sizeB = size(A)[2:end]
     return reinterpret(Complex{T}, A, sizeB)
 end
+"Convert a complex array of size dims to a real array of size 2 x dims"
 function complex_to_real(B::Array{Complex{T}}) where T<:Real
     sizeA = tuple(2,size(B)...)
     return reinterpret(T, B, sizeA)
