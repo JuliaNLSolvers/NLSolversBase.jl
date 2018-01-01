d927b3bd4688feedb6f998d7091668bd6f03acbd
Asbjørn Nilsen Riseth <anriseth@gmail.com>
2018-01-01 23:56:05 +0100

    Allocate G and H

--- src/objective_types/twicedifferentiable.jl
+++ src/objective_types/twicedifferentiable.jl
@@ -25,7 +25,7 @@ function TwiceDifferentiable(f, g!, fg!, h!, x::TX, F::T, G::TG = similar(x), H:
 end
 
 
-function TwiceDifferentiable(f, g!, h!, x::AbstractVector, F = real(zero(eltype(x))), G = similar(x))
+function TwiceDifferentiable(f, g!, h!, x::AbstractVector, F = real(zero(eltype(x))), G = similar(x), H = alloc_H(x))
     fg! = make_fdf(x, F, f, g!)
-    return TwiceDifferentiable(f, g!, fg!, h!, x, F)
+    return TwiceDifferentiable(f, g!, fg!, h!, x, F, G, H)
 end
