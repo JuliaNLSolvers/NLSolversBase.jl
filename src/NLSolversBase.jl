ae4086b30d82d805bd7c1fc6edcdfb6cc6962109
chriselrod <elrodc@gmail.com>
2018-03-31 00:44:10 -0500

    Drop reshape reinterpret, removed unnecessary nothings, fixed x of nans.

--- src/NLSolversBase.jl
+++ src/NLSolversBase.jl
@@ -41,10 +41,10 @@ export AbstractObjective,
 export AbstractConstraints, OnceDifferentiableConstraints,
     TwiceDifferentiableConstraints, ConstraintBounds
 
-function x_of_nans(x)   # https://github.com/JuliaLang/julia/issues/26516
-    out = copy(x)       # if above issue is fixed, revert.
-    (out.=(eltype(x))(NaN); nothing)
-    out
+function x_of_nans(x)
+    x_out = copy(x)
+    x_out .= (eltype(x))(NaN)
+    x_out
 end
 
 include("complex_real.jl")
--- src/complex_real.jl
+++ src/complex_real.jl
@@ -1,22 +1,22 @@
 "Convert a real array of size 2 x dims to a complex array of size dims"
 function real_to_complex(A::AbstractArray{T}) where T<:Real
     @assert size(A)[1] == 2
-    stripfirst(a, b...) = b
-    sizeB = stripfirst(size(A)...) #type-stable way of doing sizeB = size(A)[2:end]
 
     @static if VERSION >= v"0.7.0-DEV.4000"
-        return reshape(reinterpret(Complex{T}, A), sizeB)
+        return reinterpret(Complex{T}, A)
     else
+        stripfirst(a, b...) = b
+        sizeB = stripfirst(size(A)...) #type-stable way of doing sizeB = size(A)[2:end]
         return reinterpret(Complex{T}, A, sizeB)
     end
 end
 "Convert a complex array of size dims to a real array of size 2 x dims"
 function complex_to_real(B::AbstractArray{Complex{T}}) where T<:Real
-    sizeA = tuple(2,size(B)...)
 
     @static if VERSION >= v"0.7.0-DEV.4000"
-        return reshape(reinterpret(T, B), sizeA)
+        return reinterpret(T, B)
     else
+        sizeA = tuple(2,size(B)...)
         return reinterpret(T, B, sizeA)
     end
 end
