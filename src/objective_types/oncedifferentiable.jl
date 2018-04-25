6a33fc25e00fe7cbe642d8ad0a65808e389466e1
chriselrod <elrodc@gmail.com>
2018-04-25 00:08:03 -0500

    Modified once differentiable.

--- src/objective_types/oncedifferentiable.jl
+++ src/objective_types/oncedifferentiable.jl
@@ -7,7 +7,7 @@ abstract type OnceDifferentiableDynamic{TF, TDF, TX, Tcplx} <: OnceDifferentiabl
 # Func untyped is preferred if you're dynamically dispatching on functions, eg
 # whether Forward or finite diff derivatives are used is unknown at compile time.
 # Used for objectives and solvers where the gradient is available/exists
-mutable struct OnceDifferentiableDynamicFuncTyped{TF, TDF, TX, Tcplx<:Union{Val{true},Val{false}},F<:Function,DF<:Function,FDF<:Union{Function,Nothing}} <: OnceDifferentiableDynamic{TF, TDF, TX, Tcplx}
+mutable struct OnceDifferentiableDynamicFuncTyped{TF, TDF, TX, Tcplx<:Union{Val{true},Val{false}},F,DF,FDF} <: OnceDifferentiableDynamic{TF, TDF, TX, Tcplx}
     f::F # objective
     df::DF # (partial) derivative of objective
     fdf::FDF # objective and (partial) derivative of objective
@@ -33,20 +33,32 @@ iscomplex(obj::OnceDifferentiable{T,Tgrad,A,Val{true}}) where {T,Tgrad,A} = true
 iscomplex(obj::OnceDifferentiable{T,Tgrad,A,Val{false}}) where {T,Tgrad,A} = false
 
 # Compatibility with old constructor that doesn't have the complex field
-function OnceDifferentiable(f, df, fdf, x::AbstractArray, F::Union{Real, AbstractArray}, ::Val{S}) where S#;
-#                            inplace = true) #inplace currently unused.
+function OnceDifferentiable(f, df, fdf, x::AbstractArray{T}, F::Union{Real, AbstractArray}, ::Val{S}) where {S,T}#;
+    #                            inplace = true) #inplace currently unused.
+        
+    iscomplex = T <: Complex
     DF = alloc_DF(x, F)
-    iscomplex = eltype(x) <: Complex
-    if iscomplex
-        x = complex_to_real(x)
-        DF = complex_to_real(DF)
-    end
 
     x_f, x_df = x_of_nans(x), x_of_nans(x)
     if S
-        OnceDifferentiableDynamicFuncTyped{typeof(F),typeof(DF),typeof(x),Val{iscomplex},typeof(f),typeof(df),typeof(fdf)}(f, df, fdf, copy(F), DF, x_f, x_df, [0,], [0,])
+        OnceDifferentiableDynamicFuncTyped{typeof(F),typeof(DF),typeof(x),Val{false},typeof(f),typeof(df),typeof(fdf)}(f, df, fdf, copy(F), DF, x_f, x_df, [0,], [0,])
     else
-        OnceDifferentiableDynamicFuncUntyped{typeof(F),typeof(DF),typeof(x),Val{iscomplex}}(f, df, fdf,
+        OnceDifferentiableDynamicFuncUntyped{typeof(F),typeof(DF),typeof(x),Val{false}}(f, df, fdf,
+                                                copy(F), copy(DF), x_f, x_df, [0,], [0,])
+    end
+end
+function OnceDifferentiable(f, df, fdf, x::AbstractArray{T}, F::Union{Real, AbstractArray}, ::Val{S}) where {S,T <: Complex}#;
+    #                            inplace = true) #inplace currently unused.
+        
+    iscomplex = T <: Complex
+    DF = complex_to_real(alloc_DF(x, F))
+    x = complex_to_real(x)
+
+    x_f, x_df = x_of_nans(x), x_of_nans(x)
+    if S
+        OnceDifferentiableDynamicFuncTyped{typeof(F),typeof(DF),typeof(x),Val{true},typeof(f),typeof(df),typeof(fdf)}(f, df, fdf, copy(F), DF, x_f, x_df, [0,], [0,])
+    else
+        OnceDifferentiableDynamicFuncUntyped{typeof(F),typeof(DF),typeof(x),Val{true}}(f, df, fdf,
                                                 copy(F), copy(DF), x_f, x_df, [0,], [0,])
     end
 end
