673935cd0b6a86f5397495079467a00252023c66
Patrick Kofod Mogensen <patrick.mogensen@gmail.com>
2018-05-01 16:03:52 +0200

    Test TwiceDifferentiableHW

--- test/runtests.jl
+++ test/runtests.jl
@@ -31,6 +31,13 @@ function exponential_hessian!(storage, x)
     storage[2, 2] = 2.0 * exp((3.0 - x[1])^2) * (2.0 * x[2]^2 - 12.0 * x[2] + 19)
 end
 
+function exponential_hessian_product!(storage, x)
+    storage[1, 1] = 2.0 * exp((2.0 - x[1])^2) * (2.0 * x[1]^2 - 8.0 * x[1] + 9)
+    storage[1, 2] = 0.0
+    storage[2, 1] = 0.0
+    storage[2, 2] = 2.0 * exp((3.0 - x[1])^2) * (2.0 * x[2]^2 - 12.0 * x[2] + 19)
+end
+
 include("objective_types.jl")
 include("interface.jl")
 include("incomplete.jl")
