6740c0a1b521239faf52c7cce249ecf70172b8a2
Patrick Kofod Mogensen <patrick.mogensen@gmail.com>
2020-01-30 11:01:08 +0100

    fix complex gradients.

--- src/objective_types/abstract.jl
+++ src/objective_types/abstract.jl
@@ -18,6 +18,6 @@ end
 # Initialize an n-by-n Jacobian
 alloc_DF(x, F) = fill(eltype(F)(NaN), length(F), length(x))
 # Initialize a gradient shaped like x
-alloc_DF(x, F::T) where T<:Number = x_of_nans(x, T)
+alloc_DF(x, F::T) where T<:Number = x_of_nans(x, promote_type(eltype(x), T))
 # Initialize an n-by-n Hessian
-alloc_H(x, F::T) where T<:Number = alloc_DF(x, T.(x))
+alloc_H(x, F::T) where T<:Number = alloc_DF(x, promote_type(eltype(x), T).(x))
